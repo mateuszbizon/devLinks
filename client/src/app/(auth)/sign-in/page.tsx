83c76f1c9440d0cea2bd74787305e03d2bc63203
Mateusz Bizoń <92033267+mateuszbizon@users.noreply.github.com>
2024-12-13 16:36:46 +0100

    sign in page

--- client/src/app/(auth)/sign-in/page.tsx
+++ client/src/app/(auth)/sign-in/page.tsx
@@ -1,8 +1,24 @@
+import LogoLargeIcon from '@/components/icons/LogoLargeIcon'
+import Link from 'next/link'
 import React from 'react'
 
 function SignInPage() {
   return (
-    <div>SignInPage</div>
+    <div>
+      <div className='flex md:justify-center mb-12'>
+        <LogoLargeIcon />
+      </div>
+      <div className='md:bg-white rounded-xl md:p-8'>
+        <div className='space-y-2 mb-10'>
+          <h1 className='text-3xl text-grey-dark'>Login</h1>
+          <p className='text-base text-grey'>Add your details below to get back into the app</p>
+        </div>
+        <div className='mb-5'></div>
+        <div className='flex justify-center'>
+          <span className='text-base text-grey'>Don't have an account? <Link href={"/sign-up"} className='link'>Create account</Link></span>
+        </div>
+      </div>
+    </div>
   )
 }
 
