521533448d796f2528a8cf396032bca9136a91e0
Mateusz Bizoń <92033267+mateuszbizon@users.noreply.github.com>
2024-12-13 20:49:39 +0100

    sign up page

---
+++ client/src/app/(auth)/sign-up/page.tsx
@@ -0,0 +1,28 @@
+import LogoLargeIcon from '@/components/icons/LogoLargeIcon'
+import Link from 'next/link'
+import React from 'react'
+
+function SignUpPage() {
+  return (
+    <div>
+        <div className='flex md:justify-center mb-12'>
+            <LogoLargeIcon />
+        </div>
+        <div className='md:bg-white rounded-xl md:p-8'>
+            <div className='space-y-2 mb-10'>
+                <h1 className='text-3xl text-grey-dark'>Create account</h1>
+                <p className='text-base text-grey'>Let’s get you started sharing your links!</p>
+            </div>
+            <div className='mb-5'>
+                
+            </div>
+            <div className='flex justify-center flex-wrap gap-1'>
+                <span className='text-base text-grey'>Already have an account?</span>
+                <Link href={"/sign-in"} className='link text-base'>Login</Link>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default SignUpPage
