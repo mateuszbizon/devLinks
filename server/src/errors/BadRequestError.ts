4d2ebee691a860dd1fc271892abb4591752dd88c
Mateusz Bizoń <92033267+mateuszbizon@users.noreply.github.com>
2024-12-17 19:03:37 +0100

    message code optional

--- server/src/errors/BadRequestError.ts
+++ server/src/errors/BadRequestError.ts
@@ -1,15 +1,15 @@
 import { CustomError } from "./CustomError";
 
 export class BadRequestError extends CustomError {
-    constructor(message: string, messageCode: string) {
+    constructor(message: string, messageCode?: string) {
         super(message)
         this.messageCode = messageCode
         Object.setPrototypeOf(this, BadRequestError.prototype)
     }
 
-    messageCode: string
+    messageCode?: string
     statusCode: number = 400
-    writeMessage(): { message: string, messageCode: string } {
+    writeMessage(): { message: string, messageCode?: string } {
         return { message: this.message, messageCode: this.messageCode }
     }
 }
