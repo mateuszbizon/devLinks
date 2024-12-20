45ea6513dc6aac01c6b562778a684c136eb258f7
Mateusz Bizoń <92033267+mateuszbizon@users.noreply.github.com>
2024-12-20 19:59:50 +0100

    update links validation

--- server/src/constants/index.ts
+++ server/src/constants/index.ts
@@ -2,3 +2,20 @@ export const REFRESH_TOKEN = "REFRESH_TOKEN"
 export const ACCESS_TOKEN = "ACCESS_TOKEN"
 export const TOKEN = "TOKEN"
 export const USER_ID = "USER_ID"
+
+export const PLATFORM_NAMES = {
+    github: "Github",
+    frontendMentor: "Frontend Mentor",
+    twitter: "Twitter",
+    linkedIn: 'LinkedIn',
+    youtube: "YouTube",
+    facebook: "Facebook",
+    twitch: "Twitch",
+    devTo: "Dev.to",
+    codewars: "Codewars",
+    codepen: "Codepen",
+    freeCodeCamp: "freeCodeCamp",
+    gitLab: "GitLab",
+    hashnode: "Hashnode",
+    stackOverflow: "Stack Overflow",
+}
---
+++ server/src/dtos/updateLinksSchema.ts
@@ -0,0 +1,38 @@
+import { PLATFORM_NAMES } from "../constants/index";
+import { FIELD_EMPTY, URL_INCORRECT } from "../constants/validations"
+import { z } from "zod"
+
+const platformPatterns: { [key: string]: RegExp } = {
+    [PLATFORM_NAMES.github]: /^https:\/\/(www\.)?github\.com\/.+$/,
+    [PLATFORM_NAMES.frontendMentor]: /^https:\/\/(www\.)?frontendmentor\.io\/.+$/,
+    [PLATFORM_NAMES.twitter]: /^https:\/\/(www\.)?twitter\.com\/.+$/,
+    [PLATFORM_NAMES.linkedIn]: /^https:\/\/(www\.)?linkedin\.com\/.+$/,
+    [PLATFORM_NAMES.youtube]: /^https:\/\/(www\.)?youtube\.com\/.+$/,
+    [PLATFORM_NAMES.facebook]: /^https:\/\/(www\.)?facebook\.com\/.+$/,
+    [PLATFORM_NAMES.twitch]: /^https:\/\/(www\.)?twitch\.tv\/.+$/,
+    [PLATFORM_NAMES.devTo]: /^https:\/\/(www\.)?dev\.to\/.+$/,
+    [PLATFORM_NAMES.codewars]: /^https:\/\/(www\.)?codewars\.com\/.+$/,
+    [PLATFORM_NAMES.codepen]: /^https:\/\/(www\.)?codepen\.io\/.+$/,
+    [PLATFORM_NAMES.freeCodeCamp]: /^https:\/\/(www\.)?freecodecamp\.org\/.+$/,
+    [PLATFORM_NAMES.gitLab]: /^https:\/\/(www\.)?gitlab\.com\/.+$/,
+    [PLATFORM_NAMES.hashnode]: /^https:\/\/(www\.)?hashnode\.com\/.+$/,
+    [PLATFORM_NAMES.stackOverflow]: /^https:\/\/(www\.)?stackoverflow\.com\/.+$/,
+}
+
+export const updateLinksSchema = z.object({
+    profileLinks: z.array(z.object({
+        id: z.string(),
+        platform: z.string(),
+        link: z.string().min(1, FIELD_EMPTY),
+    }).refine(data => {
+        const platformPattern = platformPatterns[data.platform]
+
+        if (!platformPattern) {
+            return true
+        }
+
+        return platformPattern.test(data.link)
+    }, { message: URL_INCORRECT, path: ["link"] }))
+})
+
+export type UpdateLinksSchema = z.infer<typeof updateLinksSchema>
