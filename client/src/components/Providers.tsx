f55986fdc9cca554a5d5557bb7e9989c31383963
Mateusz Bizoń <92033267+mateuszbizon@users.noreply.github.com>
2024-12-21 15:10:11 +0100

    setup react query

--- client/src/components/Providers.tsx
+++ client/src/components/Providers.tsx
@@ -1,14 +1,19 @@
 "use client"
 
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { store } from '@/lib/store'
 import React, { PropsWithChildren } from 'react'
 import { Provider } from 'react-redux'
 
 function Providers({ children }: PropsWithChildren) {
+  const queryClient = new QueryClient()
+
   return (
-    <Provider store={store}>
+    <QueryClientProvider client={queryClient}>
+      <Provider store={store}>
         {children}
-    </Provider>
+      </Provider>
+    </QueryClientProvider>
   )
 }
 
